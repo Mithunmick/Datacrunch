ff3364afc2bad561c3d9232ed7e45d65293f59a4
Mithunmick <fazle.mithun@northsouth.edu>
2022-10-23 02:22:25 +0600

    Interface

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,8 +5,20 @@ const Home: React.FC = () => {
   const navigate = useNavigate();
   return (
     <>
-      <div>Home</div>
-      <button onClick={() => navigate('login')}>Submit</button>
+      <div className='min-h-screen bg-blue-100 flex flex-col justify-center'>
+        <div className='max-w-md w-full mx-auto bg-yellow-50'>
+          <h1 className='font-mono text-gray-500 text-center text-5xl p-3'>
+            Datacrunch
+          </h1>
+          <p className='text-green-600 text-center text-2xl p-2'>Welcome</p>
+        </div>
+        <button
+          className='max-w-md w-full mx-auto p-5 bg-gray-500 text-red-500 text-bold'
+          onClick={() => navigate('login')}
+        >
+          Click me to Start
+        </button>
+      </div>
     </>
   );
 };
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -36,41 +36,60 @@ const Login = () => {
   };
 
   return (
-    <div className='container'>
-      <div className='row justify-content-center'>
-        <div className='col-4'>
-          <div className='mainform mt-3'>
-            <form onSubmit={handleSubmit}>
-              <div className='form-group'>
-                <label htmlFor='email'>Email</label>
+    <div className=' min-h-screen bg-blue-100 flex flex-col justify-center'>
+      <div className=''>
+        <div>
+          <h1 className='p-2 text-center text-3xl'>Welcome to Login Page </h1>
+          <p className='p-3 text-center text-xl'>
+            Please enter your email and password
+          </p>
+        </div>
+        <div className='max-w-md w-full mx-auto'>
+          <div className='mt-10'>
+            <form
+              className='flex justify-center flex-col'
+              onSubmit={handleSubmit}
+            >
+              <div className='mb-6'>
+                <label
+                  className='text-center block  text-xl font-medium text-gray-900 dark:text-gray-500'
+                  htmlFor='email'
+                >
+                  Email
+                </label>
                 <input
                   type='email'
                   name='email'
-                  className='form-control'
+                  className='block p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                   id='email'
                   value={value.email || ''}
                   onChange={onChangeHandler}
                 ></input>
               </div>
-              <div className='form-group'>
+              <div className='text-center block pb-3 text-xl font-medium text-gray-900 dark:text-gray-500'>
                 <label htmlFor='password'>Password</label>
                 <input
                   type='password'
                   name='password'
-                  className='form-control'
+                  className='block p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                   id='password'
                   value={value.password || ''}
                   onChange={onChangeHandler}
                 ></input>
               </div>
 
-              <button type='submit' className='btn btn-primary'>
+              <button
+                type='submit'
+                className='max-w-md block mt-5 p-4 w-full bg-gray-500 rounded-lg border hover:bg-blue-500 text-white font-bold py-2 px-4'
+              >
                 Submit
               </button>
             </form>
 
-            {emailAlert && <div>{emailAlert}</div>}
-            {passwordAlert && <div>{passwordAlert}</div>}
+            <div className='text-red-600 text-center text-2xl p-2'>
+              {emailAlert && <div>{emailAlert}</div>}
+              {passwordAlert && <div>{passwordAlert}</div>}
+            </div>
           </div>
         </div>
       </div>
